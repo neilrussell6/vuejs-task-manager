d3ad954a7f3b2763961563c0d8d26e171c907327
Neil Russell <neilrussell6@gmail.com>
2017-03-12 23:57:04 +0200

    Remove app.settings from gitignore.

--- src/data/app.settings.js
+++ src/data/app.settings.js
@@ -1,4 +1,4 @@
-export const DOMAIN = 'http://127.0.0.1:8000/';
+export const DOMAIN = 'http://task-manager-api.neilrussell.co.za/';
 export const API_PREFIX = 'api';
 export const ARTIFICIAL_DELAY = 1000;
 export const LOGIN_DELAY = 0;
